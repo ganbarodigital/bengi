c2869039af9b6c493845072235f91138c57225b1
Stuart Herbert <stuart@stuartherbert.com>
2017-09-16 10:34:44 +0100

    New: interrogate configured PHP commands, find out what we really have

---
+++ src/Helpers/BuildListOfPhpVersions.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Copyright (c) 2017-present Ganbaro Digital Ltd
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the names of the copyright holders nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author    Stuart Herbert <stuherbert@ganbarodigital.com>
+ * @copyright 2017-present Ganbaro Digital Ltd www.ganbarodigital.com
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link      http://ganbarodigital.github.io/bengi
+ */
+
+namespace GanbaroDigital\Bengi\Helpers;
+
+/**
+ * find out what PHP versions we actually have
+ */
+class BuildListOfPhpVersions
+{
+    public static function from($phpVersions)
+    {
+        // our return value
+        $retval = [];
+
+        foreach ($phpVersions as $phpVersion => $phpCmd) {
+            $shortVerFunc = function() use ($phpCmd) {
+                // let's get what we need
+                return 'PHP v' . RunPhpCmd::using($phpCmd, 'echo PHP_MAJOR_VERSION . \'.\' . PHP_MINOR_VERSION;');
+            };
+            $fullVerFunc = function() use ($phpCmd) {
+                // let's get what we need
+                return 'PHP v' . RunPhpCmd::using($phpCmd, 'echo PHP_VERSION;');
+            };
+            $retval[$phpVersion] = [
+                'shortVersion' => $shortVerFunc(),
+                'fullVersion'  => $fullVerFunc(),
+            ];
+        }
+
+        // all done
+        return $retval;
+    }
+}
